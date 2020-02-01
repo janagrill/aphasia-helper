35b83cfc1fc910e69944f2a520f8da108d20a86b
Ruben Grill <ruben@purelabs.io>
2020-02-01 15:12:26 +0100

    Implement DatePicker

--- lib/date_trainer.dart
+++ lib/date_trainer.dart
@@ -1,9 +1,13 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:intl/intl.dart';
 
 import 'round_button.dart';
 
 FlutterTts flutterTts = FlutterTts();
+DateFormat dateFormat = DateFormat.yMMMd();
 
 class DateTrainer extends StatefulWidget {
   @override
@@ -14,15 +18,32 @@ class _DateTrainerState extends State<DateTrainer> {
   DateTime selectedDate = DateTime.now();
 
   Future<Null> _selectDate(BuildContext context) async {
-    final DateTime picked = await showDatePicker(
-        context: context,
-        initialDate: selectedDate,
-        firstDate: DateTime(2015, 8),
-        lastDate: DateTime(2101));
-    if (picked != null && picked != selectedDate)
+    final DateTime date = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(1900),
+      lastDate: DateTime(2101),
+    );
+    _setSelectedDate(date);
+  }
+
+  _setSelectedDate(DateTime date) {
+    if (date != null && date != selectedDate) {
       setState(() {
-        selectedDate = picked;
+        selectedDate = date;
       });
+    }
+  }
+
+  _speak(DateTime date) {
+    flutterTts.speak(dateFormat.format(date));
+  }
+
+  _selectRandomDate() {
+    var offset = Random().nextInt(355);
+    var year = DateTime.now().year;
+    var date = DateTime(year).add(Duration(days: offset));
+    _setSelectedDate(date);
   }
 
   @override
@@ -30,26 +51,25 @@ class _DateTrainerState extends State<DateTrainer> {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
-        Text("${selectedDate.toLocal()}".split(' ')[0],
+        Text(dateFormat.format(selectedDate),
             style: TextStyle(fontSize: 30)),
+        SizedBox(height: 30),
         RoundButton(
           text: 'Select date',
           icon: Icons.date_range,
           onPressed: () => _selectDate(context),
         ),
+        SizedBox(height: 30),
         RoundButton(
           text: 'Play',
           icon: Icons.play_arrow,
-          onPressed: () {
-            flutterTts.speak("Hello World");
-          },
+          onPressed: () => _speak(selectedDate),
         ),
+        SizedBox(height: 30),
         RoundButton(
           text: 'Next',
           icon: Icons.navigate_next,
-          onPressed: () {
-            print('next button pressed');
-          },
+          onPressed: () => _selectRandomDate(),
         ),
       ],
     );
