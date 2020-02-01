1a33c2a8cd415bfc11d5b1ad94edebc6ec851db7
Jane Djumerkoski <jane.djumerkoski@flixbus.com>
2020-02-01 16:20:36 +0100

    emergency tab as 4th

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_coding_challenge/date_trainer.dart';
 
 import 'date_trainer.dart';
 import 'number_trainer.dart';
@@ -35,6 +36,10 @@ class _HomeState extends State<Home> {
           tabIndex = index;
         }),
         items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.add_alert),
+            title: new Text('Emergency'),
+          ),
           BottomNavigationBarItem(
             icon: new Icon(Icons.format_list_numbered),
             title: new Text('Numbers'),
