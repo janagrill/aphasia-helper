aa6f3fea2fb519590e44a964d98dadb518285c27
Jane Djumerkoski <jane.djumerkoski@flixbus.com>
2020-02-01 16:30:00 +0100

    emergency tab as 4th again

--- lib/home.dart
+++ lib/home.dart
@@ -37,10 +37,6 @@ class _HomeState extends State<Home> {
         }),
         items: [
           BottomNavigationBarItem(
-            icon: new Icon(Icons.add_alert),
-            title: new Text('Emergency'),
-          ),
-          BottomNavigationBarItem(
             icon: new Icon(Icons.format_list_numbered),
             title: new Text('Numbers'),
           ),
